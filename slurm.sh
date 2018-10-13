a25cb6e1f644991c6ed8c898add900d945de87ef
michalkoziarski <michalkoziarski@gmail.com>
2018-10-13 19:08:04 +0200

    slurm script

---
+++ slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:00:00
+#SBATCH -p plgrid-gpu
+#SBATCH --gres=gpu
+
+module add plgrid/tools/python/3.6.0
+module add plgrid/apps/cuda/9.0
+
+python3 ${1} ${@:2}
