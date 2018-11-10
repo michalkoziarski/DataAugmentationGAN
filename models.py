8c5791bac4e83b64db4f4f82ef78003e21cf9752
michalkoziarski <michalkoziarski@gmail.com>
2018-11-10 12:09:39 +0100

    added simple GAN for STL-10

--- models.py
+++ models.py
@@ -5,11 +5,18 @@ from abc import ABC, abstractmethod
 
 
 class AbstractModel(ABC):
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, input_shape=None, output_shape=None, inputs=None, reuse=False):
+        assert not (input_shape is None and inputs is None)
+
         self.input_shape = input_shape
         self.output_shape = output_shape
+        self.reuse = reuse
 
-        self.inputs = tf.placeholder(tf.float32, shape=[None] + list(input_shape))
+        if inputs is None:
+            self.inputs = tf.placeholder(tf.float32, shape=[None] + list(input_shape))
+        else:
+            self.inputs = inputs
+
         self.outputs = self.inputs
         self.layers = []
         self.is_training = tf.placeholder_with_default(False, [])
@@ -31,28 +38,74 @@ class AbstractModel(ABC):
 
 class Classifier(AbstractModel):
     def setup(self):
-        self.add(tf.layers.Conv2D(64, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.Conv2D(64, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
+        with tf.variable_scope('Classifier', reuse=self.reuse):
+            self.add(tf.layers.Conv2D(64, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.Conv2D(64, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
 
-        self.add(tf.layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
+            self.add(tf.layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.Conv2D(128, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
 
-        self.add(tf.layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
+            self.add(tf.layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.Conv2D(256, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
 
-        self.add(tf.layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu))
-        self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
+            self.add(tf.layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.Conv2D(512, 3, padding='same', activation=tf.nn.relu))
+            self.add(tf.layers.MaxPooling2D(2, 2, padding='same'))
 
-        self.add(tf.layers.Flatten())
+            self.add(tf.layers.Flatten())
 
-        self.add(tf.layers.Dense(1024, activation=tf.nn.relu))
-        self.add(tf.layers.Dropout(0.5))
+            self.add(tf.layers.Dense(1024, activation=tf.nn.relu))
+            self.add(tf.layers.Dropout(0.5))
 
-        self.add(tf.layers.Dense(1024, activation=tf.nn.relu))
-        self.add(tf.layers.Dropout(0.5))
+            self.add(tf.layers.Dense(1024, activation=tf.nn.relu))
+            self.add(tf.layers.Dropout(0.5))
 
-        self.add(tf.layers.Dense(self.output_shape[0], activation=tf.nn.relu))
+            self.add(tf.layers.Dense(self.output_shape[0], activation=tf.nn.relu))
+
+
+class Discriminator(AbstractModel):
+    def setup(self):
+        with tf.variable_scope('Discriminator', reuse=self.reuse):
+            self.add(tf.layers.Conv2D(64, 5, 2, 'same', activation=tf.nn.leaky_relu))
+            self.add(tf.layers.Dropout(0.4))
+
+            self.add(tf.layers.Conv2D(128, 5, 2, 'same', activation=tf.nn.leaky_relu))
+            self.add(tf.layers.Dropout(0.4))
+
+            self.add(tf.layers.Conv2D(256, 5, 2, 'same', activation=tf.nn.leaky_relu))
+            self.add(tf.layers.Dropout(0.4))
+
+            self.add(tf.layers.Conv2D(512, 5, 1, 'same', activation=tf.nn.leaky_relu))
+            self.add(tf.layers.Dropout(0.4))
+
+            self.add(tf.layers.Flatten())
+
+            self.add(tf.layers.Dense(1, activation=tf.nn.sigmoid))
+
+
+class Generator(AbstractModel):
+    def setup(self):
+        with tf.variable_scope('Generator', reuse=self.reuse):
+            self.add(tf.layers.Dense(1024 * 4 * 4))
+            self.add(tf.layers.BatchNormalization(momentum=0.9))
+            self.outputs = tf.nn.relu(self.outputs)
+
+            self.outputs = tf.reshape(self.outputs, (-1, 4, 4, 1024))
+            self.add(tf.layers.Dropout(0.4))
+
+            self.add(tf.layers.Conv2DTranspose(512, 5, 2, 'same'))
+            self.add(tf.layers.BatchNormalization(momentum=0.9))
+            self.outputs = tf.nn.relu(self.outputs)
+
+            self.add(tf.layers.Conv2DTranspose(256, 5, 2, 'same'))
+            self.add(tf.layers.BatchNormalization(momentum=0.9))
+            self.outputs = tf.nn.relu(self.outputs)
+
+            self.add(tf.layers.Conv2DTranspose(128, 5, 2, 'same'))
+            self.add(tf.layers.BatchNormalization(momentum=0.9))
+            self.outputs = tf.nn.relu(self.outputs)
+
+            self.add(tf.layers.Conv2DTranspose(3, 5, 3, 'same', activation=tf.nn.sigmoid))
---
+++ run_generation.py
@@ -0,0 +1,111 @@
+import argparse
+import imageio
+import logging
+import numpy as np
+import os
+import tensorflow as tf
+
+from containers import STL10Container
+from models import Discriminator, Generator
+from pathlib import Path
+from tqdm import tqdm
+
+
+SAMPLES_PATH = Path(__file__).parent / 'samples'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-batch_size', type=int, default=50)
+parser.add_argument('-evaluation_step', type=int, default=100)
+parser.add_argument('-iterations', type=int, default=15000)
+parser.add_argument('-learning_rate', type=float, default=0.0002)
+
+args = parser.parse_args()
+
+logging.info('Loading data...')
+
+dataset = STL10Container('train', args.batch_size)
+
+logging.info('Constructing model...')
+
+generator = Generator([100])
+discriminator = Discriminator([96, 96, 3])
+combined_gen_dis = Discriminator(inputs=generator.outputs, reuse=True)
+
+generator_loss = tf.reduce_mean(
+    tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=combined_gen_dis.outputs, labels=tf.ones_like(combined_gen_dis.outputs)
+    )
+)
+discriminator_loss_real = tf.reduce_mean(
+    tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=discriminator.outputs, labels=tf.ones_like(discriminator.outputs)
+    )
+)
+discriminator_loss_fake = tf.reduce_mean(
+    tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=combined_gen_dis.outputs, labels=tf.zeros_like(combined_gen_dis.outputs)
+    )
+)
+discriminator_loss = discriminator_loss_real + discriminator_loss_fake
+
+generator_variables = [var for var in tf.trainable_variables() if 'Generator' in var.name]
+discriminator_variables = [var for var in tf.trainable_variables() if 'Discriminator' in var.name]
+
+generator_train_step = tf.train.AdamOptimizer(args.learning_rate).\
+    minimize(generator_loss, var_list=generator_variables)
+discriminator_train_step = tf.train.AdamOptimizer(args.learning_rate).\
+    minimize(discriminator_loss, var_list=discriminator_variables)
+
+logging.info('Training model...')
+
+SAMPLES_PATH.mkdir(parents=True, exist_ok=True)
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+
+    n_epochs = args.iterations // args.evaluation_step
+
+    for i in range(n_epochs):
+        logging.info('Processing epoch %d/%d...' % (i + 1, n_epochs))
+
+        generator_losses = []
+        discriminator_losses = []
+
+        for _ in tqdm(range(args.evaluation_step)):
+            batch_images, batch_labels = dataset.batch()
+            batch_noise = np.random.uniform(-1, 1, [args.batch_size, 100]).astype(np.float32)
+
+            feed_dict = {
+                discriminator.inputs: batch_images,
+                generator.inputs: batch_noise,
+                discriminator.is_training: True,
+                generator.is_training: True
+            }
+
+            _, batch_discriminator_loss = session.run(
+                [discriminator_train_step, discriminator_loss], feed_dict=feed_dict
+            )
+            discriminator_losses.append(batch_discriminator_loss)
+
+            for _ in range(2):
+                _, batch_generator_loss = session.run(
+                    [generator_train_step, generator_loss], feed_dict=feed_dict
+                )
+
+                generator_losses.append(batch_generator_loss)
+
+        logging.info('Observed generator loss = %.4f and discriminator loss = %.4f.' %
+                     (np.mean(generator_losses), np.mean(discriminator_losses)))
+        logging.info('Generating images...')
+
+        generated_images = session.run([generator.outputs], feed_dict={
+            generator.inputs: np.random.uniform(-1, 1, [args.batch_size, 100]).astype(np.float32)
+        })[0]
+
+        for j in range(len(generated_images)):
+            imageio.imwrite(str(SAMPLES_PATH / ('%d.png' % j)), generated_images[j])
