44739c998090abe69926a778d09991531924cb7a
michalkoziarski <michalkoziarski@gmail.com>
2018-11-20 20:14:00 +0100

    fixed batch norm training bug; added different datasets; slight refactor

--- run_generation.py
+++ run_generation.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import tensorflow as tf
 
-from containers import STL10Container
+from containers import CIFAR10Container, MNISTContainer, STL10Container
 from models import Discriminator, Generator
 from pathlib import Path
 from tqdm import tqdm
@@ -19,36 +19,46 @@ logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-batch_size', type=int, default=50)
-parser.add_argument('-evaluation_step', type=int, default=100)
-parser.add_argument('-iterations', type=int, default=15000)
+parser.add_argument('-batch_size', type=int, default=100)
+parser.add_argument('-dataset', type=str, choices=['cifar10', 'mnist', 'stl10'], required=True)
+parser.add_argument('-evaluation_step', type=int, default=50)
+parser.add_argument('-iterations', type=int, default=7500)
 parser.add_argument('-learning_rate', type=float, default=0.0002)
 
 args = parser.parse_args()
 
 logging.info('Loading data...')
 
-dataset = STL10Container('train', args.batch_size)
+if args.dataset == 'cifar10':
+    Container = CIFAR10Container
+elif args.dataset == 'mnist':
+    Container = MNISTContainer
+elif args.dataset == 'stl10':
+    Container = STL10Container
+else:
+    raise NotImplementedError
+
+dataset = Container('train', args.batch_size, image_size=[64, 64], greyscale=True)
 
 logging.info('Constructing model...')
 
-generator = Generator([100])
-discriminator = Discriminator([96, 96, 3])
-combined_gen_dis = Discriminator(inputs=generator.outputs, reuse=True)
+generator = Generator([1, 1, 100])
+real_discriminator = Discriminator([64, 64, 1])
+fake_discriminator = Discriminator(inputs=generator.outputs, reuse=True)
 
 generator_loss = tf.reduce_mean(
     tf.nn.sigmoid_cross_entropy_with_logits(
-        logits=combined_gen_dis.outputs, labels=tf.ones_like(combined_gen_dis.outputs)
+        logits=fake_discriminator.logits, labels=tf.ones([args.batch_size, 1, 1, 1])
     )
 )
 discriminator_loss_real = tf.reduce_mean(
     tf.nn.sigmoid_cross_entropy_with_logits(
-        logits=discriminator.outputs, labels=tf.ones_like(discriminator.outputs)
+        logits=real_discriminator.logits, labels=tf.ones([args.batch_size, 1, 1, 1])
     )
 )
 discriminator_loss_fake = tf.reduce_mean(
     tf.nn.sigmoid_cross_entropy_with_logits(
-        logits=combined_gen_dis.outputs, labels=tf.zeros_like(combined_gen_dis.outputs)
+        logits=fake_discriminator.logits, labels=tf.zeros([args.batch_size, 1, 1, 1])
     )
 )
 discriminator_loss = discriminator_loss_real + discriminator_loss_fake
@@ -56,10 +66,13 @@ discriminator_loss = discriminator_loss_real + discriminator_loss_fake
 generator_variables = [var for var in tf.trainable_variables() if 'Generator' in var.name]
 discriminator_variables = [var for var in tf.trainable_variables() if 'Discriminator' in var.name]
 
+generator_weights = [var for var in generator_variables if 'kernel' in var.name]
+discriminator_weights = [var for var in discriminator_variables if 'kernel' in var.name]
+
 with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-    generator_train_step = tf.train.AdamOptimizer(args.learning_rate).\
+    generator_train_step = tf.train.AdamOptimizer(args.learning_rate, beta1=0.5).\
         minimize(generator_loss, var_list=generator_variables)
-    discriminator_train_step = tf.train.AdamOptimizer(args.learning_rate).\
+    discriminator_train_step = tf.train.AdamOptimizer(args.learning_rate, beta1=0.5).\
         minimize(discriminator_loss, var_list=discriminator_variables)
 
 logging.info('Training model...')
@@ -79,13 +92,14 @@ with tf.Session() as session:
 
         for _ in tqdm(range(args.evaluation_step)):
             batch_images, batch_labels = dataset.batch()
-            batch_noise = np.random.uniform(-1, 1, [args.batch_size, 100]).astype(np.float32)
+            batch_noise = np.random.uniform(-1, 1, [args.batch_size, 1, 1, 100]).astype(np.float32)
 
             feed_dict = {
-                discriminator.inputs: batch_images,
                 generator.inputs: batch_noise,
-                discriminator.is_training: True,
-                generator.is_training: True
+                real_discriminator.inputs: batch_images,
+                generator.is_training: True,
+                real_discriminator.is_training: True,
+                fake_discriminator.is_training: True
             }
 
             _, batch_discriminator_loss = session.run(
@@ -105,7 +119,7 @@ with tf.Session() as session:
         logging.info('Generating images...')
 
         generated_images = session.run([generator.outputs], feed_dict={
-            generator.inputs: np.random.uniform(-1, 1, [args.batch_size, 100]).astype(np.float32)
+            generator.inputs: np.random.uniform(-1, 1, [args.batch_size, 1, 1, 100]).astype(np.float32)
         })[0]
 
         generated_images /= 2.0
