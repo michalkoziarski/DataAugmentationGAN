55a36738f3cdae876c8d15eac00f49d5c6f6d133
michalkoziarski <michal.koziarski@gmail.com>
2018-11-30 19:08:34 +0100

    queuing experiments for all datasets instead of a single dataset passed as a command line argument

--- queue_second_experiment.sh
+++ queue_second_experiment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for iteration in 1 2 3 4 5 6 7 8 9 10
+for dataset in cifar10 mnist stl10
 do
-    sbatch --output=$0_scale+translation_${iteration}.out slurm.sh run_training.py -augmentations scale translation -name_suffix ${iteration} -dataset $0
-    sbatch --output=$0_rotation+scale_${iteration}.out slurm.sh run_training.py -augmentations rotation scale -name_suffix ${iteration} -dataset $0
-    sbatch --output=$0_rotation+translation_${iteration}.out slurm.sh run_training.py -augmentations rotation translation -name_suffix ${iteration} -dataset $0
-    sbatch --output=$0_rotation+scale+translation_${iteration}.out slurm.sh run_training.py -augmentations rotation scale translation -name_suffix ${iteration} -dataset $0
-    sbatch --output=$0_flip+rotation+scale+translation_${iteration}.out slurm.sh run_training.py -augmentations flip rotation scale translation -name_suffix ${iteration} -dataset $0
+    for iteration in 1 2 3 4 5 6 7 8 9 10
+    do
+        sbatch --output=${dataset}_scale+translation_${iteration}.out slurm.sh run_training.py -augmentations scale translation -name_suffix ${iteration} -dataset ${dataset}
+        sbatch --output=${dataset}_rotation+scale_${iteration}.out slurm.sh run_training.py -augmentations rotation scale -name_suffix ${iteration} -dataset ${dataset}
+        sbatch --output=${dataset}_rotation+translation_${iteration}.out slurm.sh run_training.py -augmentations rotation translation -name_suffix ${iteration} -dataset ${dataset}
+        sbatch --output=${dataset}_rotation+scale+translation_${iteration}.out slurm.sh run_training.py -augmentations rotation scale translation -name_suffix ${iteration} -dataset ${dataset}
+        sbatch --output=${dataset}_flip+rotation+scale+translation_${iteration}.out slurm.sh run_training.py -augmentations flip rotation scale translation -name_suffix ${iteration} -dataset ${dataset}
+    done
 done
